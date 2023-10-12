aeccfeb4e292fa245d5ceef658d7f3a5b174c420
Mayatskiy Anton <work.mayatskiy@gmail.com>
2023-10-12 16:55:47 +1000

    refactor: move cloudinary plugin to production

--- config/plugins.ts
+++ config/plugins.ts
@@ -26,20 +26,5 @@ module.exports = ({ env }) => ({
         config: {
             defaultDepth: 5, // Default is 5
         }
-    },
-    upload: {
-        config: {
-            provider: 'cloudinary',
-            providerOptions: {
-                cloud_name: env('CLOUDINARY_NAME'),
-                api_key: env('CLOUDINARY_KEY'),
-                api_secret: env('CLOUDINARY_SECRET'),
-            },
-            actionOptions: {
-                upload: {},
-                uploadStream: {},
-                delete: {},
-            },
-        },
-    },
+    }
 });
